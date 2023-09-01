83ca7553dd2b8825bda8ee1a7872e7ed8a78cbb3
Prince Raj <119520771+Prince9558@users.noreply.github.com>
2023-09-02 00:05:17 +0530

    Add files via upload

---
+++ hex oct dec.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i;
+	cout<<"enter hexadecimal number: ";
+	cin>>hex>>i;
+	cout<<"hexadecimal value :"<<hex<<i<<endl;
+	cout<<"octal value :"<<oct<<i<<endl;
+	cout<<"decimal value :"<<dec<<i<<endl;
+	return 0;
+}
