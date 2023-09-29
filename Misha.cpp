70839402d50d5c6a3b2bbde0612d3dcfd6a27d9f
Prince Raj <119520771+Prince9558@users.noreply.github.com>
2023-09-29 15:12:38 +0530

    Add files via upload

---
+++ Misha.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+bool is_open(){
+    bool close();
+}
+
+int main(){
+
+    string s;
+    cin>>s;
+
+    cout<<"File '"<<s<<"' opened successfully."<<endl;
+    cout<<"File '"<<s<<"' closed successfully.";
+
+    return 0;
+}
+
