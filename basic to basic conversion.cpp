ad01a4275e3ff45c1316fb2a56575c5f18ba2434
Prince Raj <119520771+Prince9558@users.noreply.github.com>
2023-10-04 08:12:06 +0530

    Add files via upload

---
+++ basic to basic conversion.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	float b = 10.25;
+	char c = 'A';
+	a=b;
+	cout<<"a = "<<a<<"b = "<<b<<endl;
+	a = c;
+	cout<<"a = "<<a<<"c = "<<c<<endl;
+}
